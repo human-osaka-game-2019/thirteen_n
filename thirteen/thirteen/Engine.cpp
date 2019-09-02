98916dcd19559877119aa2e740f7d1a65ba11cbd
mono1140 <11potato30@gmail.com>
2019-09-02 20:08:20 +0900

    Engine.cpp Engine.h の追加

--- thirteen/thirteen/Engine.cpp
+++ thirteen/thirteen/Engine.cpp
@@ -1,1 +1,69 @@
-﻿
+﻿#include "class.h"
+#include "window.h"
+#include "Device.h"
+#include "Engine.h"
+
+// ポインタ宣言をまとめたクラス
+//Pointa Point;
+
+HWND* InitEngine(int width, int height, HINSTANCE hInst, DirectX* directX)
+{
+	//	window生成
+	//HWND wind_handle = Make_WndFull(hInst,width,height);
+	HWND hWnd = NULL;
+	static CHAR szAppName[] = "Pac_Man";
+	FLOAT wnd_width = width;
+	FLOAT wnd_height = height;
+	OutputWindow(&hWnd, &hInst, szAppName, &wnd_width, &wnd_height);
+
+	if (FAILED(BuildDxDevice(&hWnd, directX)))
+	{
+
+		return 0;
+
+	}
+	return &hWnd;
+}
+
+void EndEngine(DirectX directX)
+{
+	// テクスチャの開放もする
+
+	/*for (int a = 0; a < TextureList::MaxTexture; a++)
+	{
+		point.pTexture[a]->Release();
+		point.pTexture[a] = nullptr;
+	}*/
+
+	if (directX.pDinput != nullptr)
+	{
+		directX.pDinput->Release();
+		directX.pDinput = nullptr;
+	}
+	if (directX.pkey != nullptr)
+	{
+		directX.pkey->Release();
+		directX.pkey = nullptr;
+	}
+}
+
+bool DrawStart(DirectX* directX)
+{
+	directX->pDevice->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0x00, 0x00, 0x00), 1.0f, 0);
+
+	directX->pDevice->SetRenderState(D3DRS_ALPHABLENDENABLE, true);
+	directX->pDevice->SetRenderState(D3DRS_SRCBLEND, D3DBLEND_SRCALPHA);
+	directX->pDevice->SetRenderState(D3DRS_DESTBLEND, D3DBLEND_INVSRCALPHA);
+
+	if (D3D_OK == directX->pDevice->BeginScene())
+	{
+		return true;
+	}
+
+	return false;
+}
+
+void UpdateInput(DirectX* directX)
+{
+	UpdateKeyStatus(directX);
+}
--- thirteen/thirteen/Engine.h
+++ thirteen/thirteen/Engine.h
@@ -1,1 +1,16 @@
-﻿
+﻿#ifndef ENGINE_H
+#define ENGINE_H
+
+#include "class.h"
+
+//Pointa Point;
+
+HWND* InitEngine(int width, int height, HINSTANCE hInst, DirectX* directX);
+
+void EndEngine(DirectX directX);
+
+bool DrawStart(DirectX* directX);
+
+void UpdateInput(DirectX* directX);
+
+#endif // !1
