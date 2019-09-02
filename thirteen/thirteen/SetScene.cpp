bcc6f3a149564f90c709bfeec2bea6f25392897f
mono1140 <11potato30@gmail.com>
2019-09-02 20:10:07 +0900

    SetScene.cpp SetScene.h の追加

--- thirteen/thirteen/SetScene.cpp
+++ thirteen/thirteen/SetScene.cpp
@@ -1,1 +1,80 @@
-﻿
+﻿#include<stdio.h>
+#include"Scene.h"
+#include"class.h"
+#include"DrawTexture.h"
+#include"Device.h"
+#include"SetScene.h"
+
+void DrawSetScene(DirectX* directX, Count* count, VariableNumber* var);
+void InitSetScene(DirectX* directX);
+void ResetSetScene(Count* count, VariableNumber* var);
+void UpdateSetScene();
+SceneId FinisSetScene();
+
+VariableNumber var;
+static bool KeyFlag = false;
+
+TEXTUREDATA SetTextureData;
+
+SceneId SetSceneMain(DirectX* directX, Count* count, VariableNumber* var)
+{
+	switch (GetCurrentSceneStep())
+	{
+		// 初期化
+	case SceneStep::InitStep:
+		InitSetScene(directX);
+		break;
+		// 本編
+	case SceneStep::MainStep:
+		UpdateSetScene();
+		break;
+		// 終了
+	case SceneStep::EndStep:
+		ResetSetScene(count, var);
+		return FinisSetScene();
+		break;
+	}
+	return SceneId::SetScene;
+}
+
+// 描画設定等
+void DrawSetScene(DirectX* directX, Count* count, VariableNumber* var)
+{
+	//DrawTest(1092, 816, 274, 86, size.normal_s_tu2, size.normal_s_tv, size.normal_e_tu2, size.normal_e_tv, &SetTextureData.m_pTexture[SetTextureList::levelselectTexture], *point);
+
+}
+void ResetSetScene(Count* count, VariableNumber* var)
+{
+};
+
+void InitSetScene(DirectX* directXt)
+{
+	// LoadTexture("Texture/bg1.png", &SetTextureData.m_pTexture[SetTextureList::BackTexture], 0, point);
+
+	ChangeSceneStep(SceneStep::MainStep);
+}
+
+// 次のシーンに行くための条件記入
+void UpdateSetScene()
+{
+	if (GetKeyStatus(DIK_RETURN))
+	{
+
+	}
+
+	ChangeSceneStep(SceneStep::EndStep);
+}
+
+// 次に飛ぶシーン先の設定
+SceneId FinisSetScene()
+{
+
+	for (int a = 0; a < SetTextureList::MaxSetTexture; a++)
+	{
+		SetTextureData.m_pTexture[a]->Release();
+		SetTextureData.m_pTexture[a] = nullptr;
+	}
+
+	return SceneId::GameScene;
+
+}
