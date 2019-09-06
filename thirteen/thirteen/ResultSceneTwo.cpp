b7467092f25f301cb027c7f4493a3aca3f52c2ab
mono1140 <11potato30@gmail.com>
2019-09-06 15:19:17 +0900

    リザルト２画面の横スクロールの追加

--- thirteen/thirteen/ResultSceneTwo.cpp
+++ thirteen/thirteen/ResultSceneTwo.cpp
@@ -4,16 +4,18 @@
 #include"DrawTexture.h"
 #include"Device.h"
 
+ResultTwoTextureSize ResultTexTwo;
 
 void DrawResultSceneTwo(DirectX* directX, Count* count  );
 void InitResultSceneTwo(DirectX* directX);
-void ResetResultSceneTwo(Count* count  );
-void UpdateResultSceneTwo();
+void UpdateResultSceneTwo(ResultTwoTextureSize *ResultTexTwo);
 SceneId FinishResultSceneTwo();
 
 static bool KeyFlag = false;
 
 TEXTUREDATA ResultTwoTextureData;
+
+
 
 SceneId ResultSceneTwoMain(DirectX* directX, Count* count  )
 {
@@ -25,34 +27,41 @@ SceneId ResultSceneTwoMain(DirectX* directX, Count* count  )
 		break;
 		// 本編
 	case SceneStep::MainStep:
-		UpdateResultSceneTwo();
+		UpdateResultSceneTwo(&ResultTexTwo);
 		break;
 		// 終了
 	case SceneStep::EndStep:
-		ResetResultSceneTwo(count );
 		return FinishResultSceneTwo();
-		break;
 	}
 	return SceneId::ResultSceneTwo;
 }
 
+void ResultTwoTextureSize::InputTu(int Tu, int TuSize)
+{
+	m_pos_tu = (float)Tu / 2560;
+	m_tu_size = (float)TuSize / 2560;
+}
 // 描画設定等
 void DrawResultSceneTwo(DirectX* directX, Count* count  )
 {
+	DrawTest(0, 0, 1280, 960, ResultTexTwo.m_pos_tu, 0, ResultTexTwo.m_tu_size, 1, &ResultTwoTextureData.m_pTexture[ResultTwoTextureList::MainbackTexture], *directX);
 }
-void ResetResultSceneTwo(Count* count  )
-{
-};
 
-void InitResultSceneTwo(DirectX* directXt)
+void InitResultSceneTwo(DirectX* directX)
 {
+	LoadTexture("Texture/result_2.png", &ResultTwoTextureData.m_pTexture[ResultTwoTextureList::MainbackTexture], 0, directX);
+
 	ChangeSceneStep(SceneStep::MainStep);
 }
 
 
 // 次のシーンに行くための条件記入
-void UpdateResultSceneTwo()
+void UpdateResultSceneTwo(ResultTwoTextureSize *p_ResultTexTwo)
 {
+	p_ResultTexTwo->m_input_tu += 1;
+	p_ResultTexTwo->m_input_tu_size += 1;
+	ResultTexTwo.InputTu(p_ResultTexTwo->m_input_tu, p_ResultTexTwo->m_input_tu_size);
+
 	if (GetKeyStatus(DIK_P))
 	{
 		ChangeSceneStep(SceneStep::EndStep);
--- thirteen/thirteen/ResultSceneTwo.h
+++ thirteen/thirteen/ResultSceneTwo.h
@@ -9,11 +9,21 @@ class ResultTwoTextureSize
 {
 public:
 
+	int m_input_tu = 0;
+	int m_input_tu_size = 1260;
+
+	float m_pos_tu  = (float) 0 / 2560;
+	float m_tu_size = (float) 1260 / 2560;
+	void InputTu(int Tu, int TuSize);
+
+	float m_pos_tv;
+	float m_tv_size;
 
 };
 
 enum ResultTwoTextureList
 {
+	MainbackTexture,
 	MaxResultTwoTexture,
 };
 
