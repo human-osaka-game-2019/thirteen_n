a0f1e3e9db307b223631d3537082c5afb209f581
mono1140 <11potato30@gmail.com>
2019-09-09 01:42:35 +0900

    タイトルシーン用のTitleSystem.cpp .h の追加
    Menu用のclass を作成
    各値を入れるための関数を作成

---
+++ thirteen/thirteen/TitleSystem.h
@@ -0,0 +1,35 @@
+﻿#ifndef TITLESYSTEM_H
+#define TITLESYSTEM_H
+
+enum 
+{
+	Up,
+	Down,
+	Left,
+	Right
+};
+
+class TitleSystem
+{
+public:
+	int TitleSceneFlag;
+	int KeyState;
+};
+
+class SelectTexture
+{
+public:
+	float m_pos_x;
+	float m_x_size;
+	float m_pos_y;
+	float m_y_size;
+	float m_pos_tu;
+	float m_tu_size;
+	float m_pos_tv;
+	float m_tv_size;
+
+	void InputTextureStatus(float x, float y, float x_size, float y_size, float tu, float tv, float tu_size, float tv_size);
+	void InputTu(float tu, float tu_size);
+};
+
+#endif // !TITLEINPUTKEY
